6e7428630d9cd95f6a3e0fc731a0c019e006bdda
jalliet <joshua.alliet@gmail.com>
2025-01-24 11:47:16 +0000

    Completed 1.2

--- Part 1 - React/courseinfo/src/App.jsx
+++ Part 1 - React/courseinfo/src/App.jsx
@@ -1,19 +1,28 @@
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
-  const exercises1 = 10
+  const ex1 = 10
   const part2 = 'Using props to pass data'
-  const exercises2 = 7
+  const ex2 = 7
   const part3 = 'State of a component'
-  const exercises3 = 14
+  const ex3 = 14
+
+  const courseParts = [
+    { part: part1, exercise: ex1 },
+    { part: part2, exercise: ex2 },
+    { part: part3, exercise: ex3 }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
-      <Total exsTotal={exercises1 + exercises2 + exercises3} />
+      {/*
+      <Content part={part1} exercise={ex1} />
+      <Content part={part2} exercise={ex2} />
+      <Content part={part3} exercise={ex3} />
+      */}
+      <RefactoredContent parts={courseParts} />
+      <Total exsTotal={ex1 + ex2 + ex3} />
     </div>
   )
 }
@@ -27,7 +36,6 @@ const Header = (props) => {
 
 }
 
-
 const Content = (props) => {
   console.log("Content component log:")
   console.log(props)
@@ -36,6 +44,35 @@ const Content = (props) => {
       <p>
         {props.part} {props.exercise}
       </p>
+    </>
+  )
+}
+
+const RefactoredContent = ({ parts }) => {
+  console.log("RefactoredContent component log:")
+  console.log(parts)
+  return (
+    <>
+      {parts.map(element => (
+        <Part key={element.part} part={element.part} exercise={element.exercise} />
+      ))}
+      {/*
+      parts.map(element => (
+        <p key={element.part}>{element.part} {element.exercise}</p>
+      ))*/}
+      {/*
+      <Part part={parts[0].part} exercise={parts[0].exercise} />
+      <Part part={parts[1].part} exercise={parts[1].exercise} />
+      <Part part={parts[2].part} exercise={parts[2].exercise} />
+      */}
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.part} {props.exercise}</p>
     </>
   )
 }
