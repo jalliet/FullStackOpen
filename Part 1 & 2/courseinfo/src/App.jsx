f183a144c5c87799750269685ddbb363a24e7a72
jalliet <joshua.alliet@gmail.com>
2025-01-26 23:41:37 +0000

    Completed 2.2

--- Part 1 & 2/courseinfo/src/App.jsx
+++ Part 1 & 2/courseinfo/src/App.jsx
@@ -34,25 +34,35 @@ const Course = ({ course }) => {
   )
 }
 
-
 const Header = ({ course }) => {
   return (
     <h1>{course.name}</h1>
   )
 }
 
-
 const Content = ({ parts }) => {
   return (
-    parts.map((part, idx) => (
-      <Part key={idx} part={part} />
-    ))
+    <>
+      {parts.map((part, idx) => (
+        <Part key={idx} part={part} />
+      ))}
+      <Total parts={parts} />
+    </>
   )
 }
 
 const Part = ({ part }) => {
   return (
     <p>{part.name}: {part.exercises}</p>
+  )
+}
+
+const Total = ({ parts }) => {
+  let total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return (
+    <p>
+      <strong>Total of {total} exercises</strong>
+    </p>
   )
 }
 
